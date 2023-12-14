af3153ae4d318d675759bfdef393e857c23144ee
--show-origin <karolm22.km@gmail.com>
2023-12-14 18:20:40 +0100

    jaj

---
+++ app/src/main/java/com/example/wikimarvel/api/CharactersParser.java
@@ -0,0 +1,2 @@
+package com.example.wikimarvel.api;public class CharactersParser {
+}
--- app/src/main/java/com/example/wikimarvel/data/characters/Character.java
+++ app/src/main/java/com/example/wikimarvel/data/characters/Character.java
@@ -9,16 +9,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Characters {
     private int id;
     private String name;
